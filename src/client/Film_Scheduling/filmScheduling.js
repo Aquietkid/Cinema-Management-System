3c638033c022c44752d83f21a43d3cf4da7ec406
Ali Imran <93790085+Aquietkid@users.noreply.github.com>
2024-05-15 21:05:57 +0500

    Film scheduling now works.

--- src/client/Film_Scheduling/filmScheduling.js
+++ src/client/Film_Scheduling/filmScheduling.js
@@ -2,10 +2,104 @@
 function setMinDate() {
     // Get today's date
     var today = new Date().toISOString().split('T')[0];
-    
+
     // Set the minimum date for the input field
     document.getElementById("FilmDate").setAttribute("min", today);
 }
 
+function fetchFilms() {
+    fetch('http://localhost:20419/filmManagement/allFilms')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            // console.log('Above return response.json()', response);
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            // Populate the dropdown with items from the backend
+            populateDropdown(data.data);
+        })
+        .catch(error => {
+            console.error('There was a problem fetching films:', error);
+        });
+}
 
+// Function to populate the dropdown with items
+function populateDropdown(films) {
+    const itemDropdown1 = document.getElementById('slot1');
+    const itemDropdown2 = document.getElementById('slot2');
+
+    // Clear existing options
+    itemDropdown1.innerHTML = '';
+    itemDropdown2.innerHTML = '';
+
+    // Add default option
+    const defaultOption1 = document.createElement('option');
+    defaultOption1.value = '';
+    defaultOption1.textContent = 'Select a movie to schedule';
+
+    const empty1 = document.createElement('option');
+    empty1.value = '0';
+    empty1.textContent = 'None';
+
+    const defaultOption2 = document.createElement('option');
+    defaultOption2.value = '';
+    defaultOption2.textContent = 'Select a movie to schedule';
+
+    const empty2 = document.createElement('option');
+    empty2.value = '0';
+    empty2.textContent = 'None';
+
+    itemDropdown1.appendChild(defaultOption1);
+    itemDropdown2.appendChild(defaultOption2);
+    itemDropdown1.appendChild(empty1);
+    itemDropdown2.appendChild(empty2);
+
+    films.forEach(film => {
+        const option = document.createElement('option');
+        option.value = film.MovieID; // Assuming each item has an ID property
+        option.textContent = film.Name; // Assuming each item has a Name property
+
+        const option2 = document.createElement('option');
+        option2.value = film.MovieID; // Assuming each item has an ID property
+        option2.textContent = film.Name; // Assuming each item has a Name property
+
+        itemDropdown1.appendChild(option);
+        itemDropdown2.appendChild(option2);
+    });
+
+}
+
+async function scheduleFilms() {
+    event.preventDefault();
+    const date = document.getElementById('FilmDate').value;
+    console.log(date);
+    const id1 = document.getElementById('slot1').value;
+    console.log(id1);
+    const id2 = document.getElementById('slot2').value;
+    console.log(id2);
+
+    fetch(`http://localhost:20419/scheduleFilm/newSchedule/${date}/${id1}/${id2}`, {
+        method: "POST"
+    })
+        .then(response => {
+            alert('Films scheduled!');
+            location.reload();
+        })
+        .then(data => {
+            console.log(data);
+            // Populate the dropdown with items from the backend
+        })
+        .catch(error => {
+            console.error('There was a problem fetching films:', error);
+        });
+
+}
+
+
+
+document.getElementById('submit-button').addEventListener('click', scheduleFilms);
+window.addEventListener(onload, fetchFilms());
 document.getElementById("FilmDate").addEventListener(onload, setMinDate());
--- src/server/routes/film_scheduling.js
+++ src/server/routes/film_scheduling.js
@@ -5,21 +5,32 @@ const connection = require("../config/persistence");
 router.post('/newSchedule/:date/:slot1id/:slot2id', (req, res) => {
     connection.connect(function (err) {
         if (err) throw err;
+        const slot1id = parseInt(req.params.slot1id);
+        const slot2id = parseInt(req.params.slot2id);
         const date = req.params.date;
-        const slot1id = req.params.slot1id;
-        const slot2id = req.params.slot2id;
-        var sql = 'INSERT INTO ;'; 
-        connection.query(sql, [movieName], function (err, result) {
-            if (err) throw err;
-            else if (result.length == 0) {
-                return res.status(404).send('No film with such a name exists!');
-            }
-            else {
-                res.status(201).json({
-                    data: result,
-                    message: "Film(s) with the matching name returned"
-                });
-            }
+
+        if (slot1id > 0) {
+            const Starttime1 = date + ' 17:00:00';
+            const EndTime1 = date + ' 20:00:00';
+            var sql = 'INSERT INTO Showtime (MovieID, Starttime, EndTime) VALUES (?, ?, ?);';
+            connection.query(sql, [slot1id, Starttime1, EndTime1], function (err, result) {
+                if (err) throw err;
+            });
+        }
+
+        if (slot2id > 0) {
+            const Starttime2 = date + ' 22:00:00';
+            const EndTime2 = date + ' 01:00:00';
+
+            var sql = 'INSERT INTO Showtime (MovieID, Starttime, EndTime) VALUES (?, ?,? );';// + 
+            connection.query(sql, [slot2id, Starttime2, EndTime2], function (err, result) {
+                if (err) throw err;
+            });
+        }
+        
+        res.status(201).json({
+            message: "Films scheduled!",
+            data: result
         });
     });
 
@@ -29,7 +40,7 @@ router.get('/:movieName', (req, res) => {
     connection.connect(function (err) {
         if (err) throw err;
         const movieName = req.params.movieName;
-        var sql = 'SELECT * FROM Movie WHERE Movie.Name LIKE ?;'; 
+        var sql = 'SELECT * FROM Movie WHERE Movie.Name LIKE ?;';
         connection.query(sql, [movieName], function (err, result) {
             if (err) throw err;
             else if (result.length == 0) {
